ab18d9176285c8b247a6cb705e1def7c4f29bb3a
Levi Payne <levi.dyrek@gmail.com>
2019-04-30 21:01:57 -0400

    Improve error handling in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,15 @@ fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8001")?;
 
     for stream in listener.incoming() {
-        rust_http_server::handle_client(stream?)?;
+        match stream {
+            Ok(stream) => {
+                match rust_http_server::handle_client(stream) {
+                    Err(e) => eprintln!("Error handling client: {}", e),
+                    _ => (),
+                }
+            },
+            Err(e) => eprintln!("Connection failed: {}", e),
+        }
     }
 
     Ok(())
