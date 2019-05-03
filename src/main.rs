2e93f912e448e14473ca4a0dfc53272b0d86bdfe
Levi Payne <levi.dyrek@gmail.com>
2019-05-03 18:15:08 -0400

    Panic when default static root does not exist or if the static root does exist but is not a directory.

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,18 @@ use rust_http_server;
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8001")?;
     let static_root = match env::var("STATIC_ROOT") {
-        Ok(val) => {
-            let path = Path::new(val);
-            if !path.exists() {
-                panic!("Path set by STATIC_ROOT does not exist or can not be accessed.");
-            }
-            val
-        },
+        Ok(val) => val,
         Err(_) => "/static".to_string(),
     };
+
+    // If static root does not exist or is not a directory, terminate.
+    let path = Path::new(&static_root);
+    if !path.exists() {
+        panic!("Static root directory ({}) does not exist or can not be accessed.", &static_root);
+    }
+    if !path.is_dir() {
+        panic!("Static root ({}) is not a directory.", &static_root);
+    }
 
     for stream in listener.incoming() {
         match stream {
